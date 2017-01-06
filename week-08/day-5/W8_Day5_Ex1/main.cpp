b10169032fae325210fdfbdca77d27babeaf4e81
Blanka Kovács <kovacsblanka06@gmail.com>
2017-01-06 15:20:54 +0100

    Added star function, snowflake not working

--- week-08/day-5/W8_Day5_Ex1/main.cpp
+++ week-08/day-5/W8_Day5_Ex1/main.cpp
@@ -7,9 +7,10 @@
 
 void draw_sierpinski(draw& d, SDL_Renderer* renderer, int line_lenght);
 void draw_randomsize_sierpinski_hexagon(draw& d, SDL_Renderer* renderer, int triangle_side);
+void draw_snowflake(draw& d, SDL_Renderer* renderer, int branch_size);
+void draw_randomsize_star(draw& d, SDL_Renderer* renderer, int triangle_side);
 
 int main(int argc, char ** argv) {
-
 	bool quit = false;
 	SDL_Event event;
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -35,6 +36,12 @@ int main(int argc, char ** argv) {
 				int triangle_side = rand() % 100;
 				draw_randomsize_sierpinski_hexagon(d, renderer, triangle_side);
 			}
+			if (SDL_GetMouseState(&x, &y) & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
+				d.MoveTo(x, y);
+				srand(time(NULL));
+				int triangle_side = rand() % 100 + 10;
+				draw_randomsize_star(d, renderer, triangle_side);
+			}
 			break;
 		}
 	}
@@ -45,7 +52,8 @@ int main(int argc, char ** argv) {
 	return 0;
 }
 
-void draw_randomsize_sierpinski_hexagon(draw& d, SDL_Renderer* renderer, int triangle_side) {
+void draw_randomsize_sierpinski_hexagon(draw& d, SDL_Renderer* renderer, 
+	int triangle_side) {
 	if (triangle_side > 1) {
 		for (int j = 0; j < 6; j++) {
 			d.TurnTo(60);
@@ -66,6 +74,45 @@ void draw_sierpinski(draw& d, SDL_Renderer* renderer, int triangle_side) {
 			d.DrawBy(renderer, triangle_side);
 			d.TurnTo(120);
 			draw_sierpinski(d, renderer, triangle_side / 2);
+		}
+	}
+	return;
+}
+
+void draw_randomsize_star(draw& d, SDL_Renderer* renderer, int triangle_side) {
+	if (triangle_side > 0) {
+		for (int i = 0; i < 6; i++) {
+			int r = rand() % 255;
+			int g = rand() % 255;
+			int b = rand() % 255;
+			SDL_SetRenderDrawColor(renderer, r, g, b, 0);
+			d.DrawBy(renderer, triangle_side);
+			d.MoveBy(-(triangle_side / 3));
+			d.TurnTo(-60);
+			draw_randomsize_star(d, renderer, triangle_side / 3);
+			draw_sierpinski(d, renderer, triangle_side / 2);
+		}
+	}
+	return;
+}
+
+
+void draw_snowflake(draw& d, SDL_Renderer* renderer, int triangle_side) { //sth wrong with this, because it generalizes two snowflake
+	//and works only with numbers dividable with 3
+	if (triangle_side > 1) {
+		for (int i = 0; i < 6; i++) {
+			SDL_SetRenderDrawColor(renderer, 100, 100, 100, 0);
+			d.DrawBy(renderer, triangle_side);
+			d.MoveBy(-(triangle_side / 3));
+			d.TurnTo(-60);
+			draw_snowflake(d, renderer, triangle_side / 3);
+		}
+		for (int i = 0; i < 6; i++) {
+			SDL_SetRenderDrawColor(renderer, 200, 100, 100, 0);
+			d.DrawBy(renderer, triangle_side);
+			//d.MoveBy(- (triangle_side / 3));
+			d.TurnTo(60);
+			draw_snowflake(d, renderer, triangle_side / 3);
 		}
 	}
 	return;
