2bfc6a88ee14da2f5d99d6dfe61b236a6f2d4adc
kblanka <kovacsblanka06@gmail.com>
2017-02-07 16:59:59 +0100

    Binary Search Example

---
+++ week-15/W15_D2_Ex1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch_CSharp
+{
+  class Program
+  {
+    public static string binarySearch(int[] inputArray, int myNum)
+    {
+      int min = 0;
+      int max = inputArray.Length - 1;
+      do
+      {
+        int mid = (min + max) / 2;
+        if (myNum > inputArray[mid])
+          min = mid + 1;
+        else
+          max = mid - 1;
+        if (inputArray[mid] == myNum)
+          return "The index of the given number: " + mid + ".";
+      } while (min <= max);
+      return "Array does not contain this number.";
+    }
+    static void Main(string[] args)
+    {
+      int[] numbers = new int[7] {55, 65, 98, 88, 4, 2, 1};
+      Array.Sort(numbers);
+      int num = 4;
+      Console.WriteLine(binarySearch(numbers, num));
+    }
+  }
+}
